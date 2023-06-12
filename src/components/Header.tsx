c42e9f848baebe355ea4b17b19eec1bb66dae8fe
Rohan Khayech <rohankhayech@gmail.com>
2023-06-12 17:01:35 +0800

    Added social links.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
-import { useMediaQuery } from '@mui/material';
+import { IconButton, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles'
+import Image from 'next/image';
 
 interface HeaderProps {
     name: string,
@@ -17,26 +18,46 @@ export default function Header({name, tagline, profileImgSrc}: HeaderProps) {
     const vertical = mobile || largeScreen
 
     return (
-        <header style={{ width: "100%", paddingRight: mobile ? 32 : 0}}>
+        <header style={{ width: "100%", paddingRight: !largeScreen ? 32 : 0}}>
             <Stack
                 direction={vertical ? "column" : "row"}
-                //justifyItems={mobile ? "center" : "flex-start"}
+                width = "100%"
                 alignItems="center"
                 spacing={4}
                 marginBottom={vertical ? 2 : 0}
             >
-                <Avatar 
-                    alt={`Picture of ${name}`} 
-                    src={profileImgSrc}
-                    sx={{ width: 128, height: 128 }}
-                />
                 <Stack
-                    direction="column"
-                    alignItems={vertical ? 'center' : "flex-start" }
-                    spacing={2}
+                    direction={vertical ? "column" : "row"}
+                    width="100%"
+                    alignItems="center"
+                    spacing={4}
                 >
-                    <Typography variant="h3" textAlign={"center"}>{name}</Typography>
-                    <Typography variant="h5" textAlign={"center"}>{tagline}</Typography>
+                    <Avatar 
+                        alt={`Picture of ${name}`} 
+                        src={profileImgSrc}
+                        sx={{ width: 128, height: 128 }}
+                    />
+                    <Stack
+                        direction="column"
+                        alignItems={vertical ? 'center' : "flex-start" }
+                        spacing={2}
+                    >
+                        <Typography variant="h3" textAlign={"center"}>{name}</Typography>
+                        <Typography variant="h5" textAlign={"center"}>{tagline}</Typography>
+                    </Stack>
+                </Stack>
+                <Stack
+                    direction="row"
+                    alignItems={vertical ? "center" : "flex-end"}
+                    spacing={1}
+                    paddingRight={vertical ? 0 : 1}
+                >
+                    <IconButton href='https://github.com/rohankhayech'>
+                        <Image width={24} height={24} src={theme.palette.mode === 'light' ? "/github-mark.svg" : "/github-mark-white.svg"} alt="Github Profile"/>
+                    </IconButton>
+                    <IconButton href='https://www.linkedin.com/in/rohan-khayech-356b01228/'>
+                        <Image style={{filter: theme.palette.mode === "light" ? "invert()" : "" }} width={24*(635/540)} height={24} src="/LI-In-Bug-White.png" alt="LinkedIn Profile" />
+                    </IconButton>
                 </Stack>
             </Stack>
         </header>
